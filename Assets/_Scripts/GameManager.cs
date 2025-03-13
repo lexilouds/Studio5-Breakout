13de25f027df829aaaa34cca6143aacd7fe0fdd2
lexilouds <lexiloudiadis@gmail.com>
2025-03-13 10:44:57 -0700

    Particle explosion and explosion sound effects implementation

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -5,6 +5,8 @@ public class GameManager : SingletonMonoBehavior<GameManager>
     [SerializeField] private int maxLives = 3;
     [SerializeField] private Ball ball;
     [SerializeField] private Transform bricksContainer;
+    [SerializeField] private GameObject explosionEffect;
+    [SerializeField] private AudioClip explosionSound;
 
     private int currentBrickCount;
     private int totalBrickCount;
@@ -32,6 +34,18 @@ public class GameManager : SingletonMonoBehavior<GameManager>
         // fire audio here
         // implement particle effect here
         // add camera shake here
+
+          if (explosionEffect != null)
+        {
+            Instantiate(explosionEffect, position, Quaternion.identity);
+        }
+
+        if (explosionSound != null)
+        {
+            AudioSource.PlayClipAtPoint(explosionSound, position);
+        }
+
+
         currentBrickCount--;
         Debug.Log($"Destroyed Brick at {position}, {currentBrickCount}/{totalBrickCount} remaining");
         if(currentBrickCount == 0) SceneHandler.Instance.LoadNextScene();
