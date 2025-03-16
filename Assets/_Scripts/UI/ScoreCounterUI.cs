b08dd1467ccda15537b03782763bdf38871eaab9
Madelyn DeGruchy <144863481+maddydeg@users.noreply.github.com>
2025-03-15 21:34:53 -0700

    Updates to pulse the "Score:" text when it is  increased

--- Assets/_Scripts/UI/ScoreCounterUI.cs
+++ Assets/_Scripts/UI/ScoreCounterUI.cs
@@ -7,9 +7,13 @@ public class ScoreCounterUI : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI current;
     [SerializeField] private TextMeshProUGUI toUpdate;
+    [SerializeField] private TextMeshProUGUI scoreText;
     [SerializeField] private Transform scoreTextContainer;
-    [SerializeField] private float duration = 0.4f;
-    [SerializeField] private Ease animationCurve;
+    [SerializeField] private float durationCount = 0.4f;
+    [SerializeField] private float pulseCount = 0.2f;
+    [SerializeField] private float scaleFactor = 1.2f;
+    [SerializeField] private Ease numberCurve;
+    [SerializeField] private Ease textCurve;
 
     private float containerInitPosition;
     private float moveAmount;
@@ -27,14 +31,16 @@ public class ScoreCounterUI : MonoBehaviour
     public void UpdateScore(int score)
     {
         toUpdate.SetText($"{score}");
-        scoreTextContainer.DOLocalMoveY(containerInitPosition + moveAmount, duration).SetEase(animationCurve);
-
+        scoreTextContainer.DOLocalMoveY(containerInitPosition + moveAmount, durationCount).SetEase(numberCurve);
+        Sequence pulseSequence = DOTween.Sequence();
+        pulseSequence.Append(scoreText.transform.DOScale(Vector3.one*scaleFactor, pulseCount).SetEase(textCurve));
+        pulseSequence.Append(scoreText.transform.DOScale(Vector3.one, pulseCount).SetEase(textCurve));
         StartCoroutine(ResetScoreContainer(score));
     }
 
     private IEnumerator ResetScoreContainer(int score)
     {
-        yield return new WaitForSeconds(duration);
+        yield return new WaitForSeconds(durationCount);
         current.SetText($"{score}");
         Vector3 localPosition = scoreTextContainer.localPosition;
         scoreTextContainer.localPosition = new Vector3(localPosition.x, containerInitPosition, localPosition.z);
